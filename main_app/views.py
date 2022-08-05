eba7eda9b0b0fe2140b737a4876796cc2538347a
Kairo Ribeiro <kairo10@gmail.com>
2022-08-05 14:25:21 -0400

    finish part 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Add the Dog class & list and view function below the imports
-class Dog:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Lolo', 'tabby', 'Kinda rude.', 3),
-  Dog('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Dog('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Dog('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+from .models import Dog
 
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello Dog</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def dogs_index(request):
+  dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', { 'dogs': dogs })
+
+def dogs_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', { 'dog': dog })
